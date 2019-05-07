09ea9b6af92c3fe835fab6f0e7f5a439fd160079
Rafael Sequeira Gutierrez <rsg_2008@hotmail.es>
2019-05-06 18:41:22 -0600

    initial structure of vinet project based on react-app structure

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'; 
+import './style/App.css'; 
+
+class NavBar extends Component {
+    render (){
+        return (
+            <div>
+                
+            </div>
+        ); 
+    }
+} 
+
+export default NavBar; 
