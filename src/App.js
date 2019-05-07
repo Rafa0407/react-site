2869fd0d3cf9a6a6db1c5cfd2649d174eaba9b61
Rafael Sequeira Gutierrez <rsg_2008@hotmail.es>
2019-05-06 22:30:28 -0600

    Starting with the Navigation Bar Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import './style/App.css';
+import NavBar from './components/navbar/NavBar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-         
-        </div>
-        <p className="App-intro">
-          
-        </p>
+        <NavBar/>
       </div>
     );
   }
